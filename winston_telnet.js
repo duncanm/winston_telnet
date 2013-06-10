f074736ce2155a46e9f56bf55aadfe0cfbb02f8e
Duncan Margetts <github@duncanmargetts.com>
2013-06-11 09:14:41 +0930

    Code format cleanup

--- winston_telnet.js
+++ winston_telnet.js
@@ -1,42 +1,37 @@
 var util = require('util'),
     net = require('net'),
-  	winston = require('winston'), 
-		sockets = [],
-		server;
+    winston = require('winston'),
+    sockets = [],
+    server;
 
 var TelnetLogger = winston.transports.TelnetLogger = function (options) {
-  this.name = 'TelnetLogger';
-  this.level = options.level || 'info';
-	server = net.createServer(newSocket).listen(options.port);
+    this.name = 'TelnetLogger';
+    this.level = options.level || 'info';
+    server = net.createServer(newSocket).listen(options.port);
 };
 
 util.inherits(TelnetLogger, winston.Transport);
 
 TelnetLogger.prototype.log = function (level, msg, meta, callback) {
-	for (var i = 0; i < sockets.length; i++) {
-			sockets[i].write(msg + '\r\n');
-	}
-
-	callback(null, true);
-	};
-
-
-function closeSocket(socket) {
-	var i = sockets.indexOf(socket);
-	if (i != -1) {
-		sockets.splice(i, 1);
-	}
-}
-
-
-function newSocket(socket){
-	sockets.push(socket);
-	socket.write('Winston Telnet Server!\r\n');
-	socket.on('end', function() {
-		closeSocket(socket);
-	})
+    for (var i = 0; i < sockets.length; i++) {
+        sockets[i].write(msg + '\r\n');
+    }
+    callback(null, true);
 };
 
 
-	
-	 
+function closeSocket(socket) {
+    var i = sockets.indexOf(socket);
+    if (i != -1) {
+        sockets.splice(i, 1);
+    }
+}
+
+
+function newSocket(socket) {
+    sockets.push(socket);
+    socket.write('Winston Telnet Server!\r\n');
+    socket.on('end', function () {
+        closeSocket(socket);
+    })
+};
